d6102ea676b1be91cfb7a0ea75a0ab6147b13070
Man Khi <Fomachka93@yahoo.com>
2023-06-28 14:10:01 +0900

    small ui update

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,143 +1,123 @@
-import { HiOutlineUser } from "react-icons/hi2";
-import Logout from "../features/authentication/Logout";
-import { useNavigate } from "react-router-dom";
 import useUser from "../features/authentication/hooks/useUser";
-import { User } from "@supabase/supabase-js";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import useLogout from "../features/authentication/hooks/useLogout";
 
 export default function Headers({
   setToggleMenu,
 }: {
   setToggleMenu: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
-  const navigate = useNavigate();
   const { user } = useUser();
   const { avatar, firstName, lastName } = user?.user_metadata || {};
+  const { logout, isLoginOut } = useLogout();
   const [toggleAvatarMenu, setToggleAvatarMenu] = useState(false);
 
   return (
-    <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-      <div className="px-3 py-3 lg:px-5 lg:pl-3">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center justify-start">
-            <button
-              onClick={() => setToggleMenu((prev) => !prev)}
-              aria-controls="logo-sidebar"
-              type="button"
-              className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            >
-              <span className="sr-only">Open sidebar</span>
-              <svg
-                className="w-6 h-6"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+    <header>
+      <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+        <div className="px-3 py-3 lg:px-5 lg:pl-3">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center justify-start">
+              <button
+                onClick={() => setToggleMenu((prev) => !prev)}
+                aria-controls="logo-sidebar"
+                type="button"
+                className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               >
-                <path
-                  clipRule="evenodd"
-                  fillRule="evenodd"
-                  d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-                ></path>
-              </svg>
-            </button>
-            <a href="https://flowbite.com" className="flex ml-2 md:mr-24">
-              <img
-                src="https://flowbite.com/docs/images/logo.svg"
-                className="h-8 mr-3"
-                alt="FlowBite Logo"
-              />
-              <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
-                Flowbite
-              </span>
-            </a>
-          </div>
-          <div className="flex items-center ">
-            <div className="flex items-center ml-3">
-              <div>
-                <button
-                  type="button"
-                  className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-                  onClick={() => setToggleAvatarMenu((prev) => !prev)}
+                <span className="sr-only">Open sidebar</span>
+                <svg
+                  className="w-6 h-6"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <span className="sr-only">Open user menu</span>
-                  <img
-                    className="w-8 h-8 rounded-full"
-                    src={avatar || "/public/images/default-user.jpg"}
-                    alt="current user photo"
-                  />
-                </button>
-              </div>
-
-              <div
-                className={`z-50 ${
-                  toggleAvatarMenu ? "absolute top-10" : "hidden"
-                } right-5 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600`}
-                id="dropdown-user"
-              >
-                <div className="px-4 py-3" role="none">
-                  <p className="text-sm text-gray-900 dark:text-white" role="none">
-                    {firstName + " " + lastName}
-                  </p>
-                  <p
-                    className="text-sm font-medium text-gray-900 truncate dark:text-gray-300"
-                    role="none"
+                  <path
+                    clipRule="evenodd"
+                    fillRule="evenodd"
+                    d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+                  ></path>
+                </svg>
+              </button>
+              {/* Icon and Project Name */}
+              <a href="https://flowbite.com" className="flex ml-2 md:mr-24">
+                <img
+                  src="https://flowbite.com/docs/images/logo.svg"
+                  className="h-8 mr-3"
+                  alt="FlowBite Logo"
+                />
+                <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
+                  Flowbite
+                </span>
+              </a>
+            </div>
+            <div className="flex items-center ">
+              <div className="flex items-center ml-3">
+                <div>
+                  <button
+                    type="button"
+                    className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                    onClick={() => setToggleAvatarMenu((prev) => !prev)}
                   >
-                    {user?.email || "No email"}
-                  </p>
+                    <span className="sr-only">Open user menu</span>
+                    <img
+                      className="w-8 h-8 rounded-full"
+                      src={avatar || "/public/images/default-user.jpg"}
+                      alt="current user photo"
+                    />
+                  </button>
                 </div>
-                <ul className="py-1" role="none">
-                  <li>
-                    <NavLink
-                      to="/dashboard"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                <div
+                  className={`z-50 ${
+                    toggleAvatarMenu ? "absolute top-12 sm:top-10" : "hidden"
+                  } right-5 my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600`}
+                  id="dropdown-user"
+                >
+                  <div className="px-4 py-3" role="none">
+                    <p className="text-sm text-gray-900 dark:text-white" role="none">
+                      {firstName + " " + lastName || "User"}
+                    </p>
+                    <p
+                      className="text-sm font-medium text-gray-900 truncate dark:text-gray-300"
+                      role="none"
                     >
-                      Dashboard
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      to="/settings"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                      Settings
-                    </NavLink>
-                  </li>
-                  <li>
-                    {/* <NavLink
-                      to="/settings"
-                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                    >
-                    </NavLink> */}
-                    <Logout />
-                  </li>
-                </ul>
+                      {user?.email || "No email"}
+                    </p>
+                  </div>
+                  <ul className="py-1" role="none">
+                    <li>
+                      <NavLink
+                        to="/dashboard"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                      >
+                        Dashboard
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink
+                        to="/settings"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                      >
+                        Settings
+                      </NavLink>
+                    </li>
+                    <li>
+                      <button
+                        onClick={() => logout()}
+                        disabled={isLoginOut}
+                        className="block px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 w-full dark:hover:bg-gray-600 dark:hover:text-white"
+                      >
+                        Log Out
+                      </button>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </nav>
-    // <header className="flex gap-10 items-center justify-end bg-white py-4 px-16 border-b-1 border-gray-100">
-    //   <div className="space-x-4">
-    //     <img
-    //       className="inline-block w-10 aspect-[1] object-cover object-center rounded-lg outline-2 outline-gray-100"
-    //       src={avatar || "/public/images/default-user.jpg"}
-    //       alt={firstName || "avatar"}
-    //     />
-    //     <span className="text-lg">{firstName + " " + lastName}</span>
-    //   </div>
-    //   <ul className="flex gap-2">
-    //     <li>
-    //       <Logout />
-    //     </li>
-    //     <li>
-    //       <button onClick={() => navigate("/account")}>
-    //         <HiOutlineUser />
-    //       </button>
-    //     </li>
-    //   </ul>
-    // </header>
+      </nav>
+    </header>
   );
 }
