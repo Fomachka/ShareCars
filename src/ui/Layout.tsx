27512e33467f6a762fa5504748aceab80ec25cef
Man Khi <Fomachka93@yahoo.com>
2023-07-01 14:50:09 +0900

    minor bugs fixes

--- src/ui/Layout.tsx
+++ src/ui/Layout.tsx
@@ -7,9 +7,9 @@ import { Navigation } from "./Navigation";
 export const Layout = () => {
   return (
     <ProtectedRoute>
-      <div>
+      <div className="dark:bg-gray-800">
         <Navigation />
-        <div className="p-4 sm:ml-64 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+        <div className="p-4 sm:ml-64 mt-14 h-screen ">
           <Outlet />
         </div>
       </div>
